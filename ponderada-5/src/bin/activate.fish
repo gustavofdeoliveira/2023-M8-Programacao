7d88a53142a64a086b75c887c282e7cd31cb52b4
Gustavo Ferreira <gustavo.oliveira@sou.inteli.edu.br>
2023-11-29 19:56:15 -0300

    feat: ponderada 5

--- ponderada-5/src/bin/activate.fish
+++ ponderada-5/src/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/gustavo/Documentos/Github/2023-M8-Programacao/ponderada-4/src"
+set -gx VIRTUAL_ENV "/home/gustavo/Documentos/Github/2023-M8-Programacao/ponderada-5/src"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
--- ponderada-5/src/main.py
+++ ponderada-5/src/main.py
@@ -1,21 +1,42 @@
-import random
 import gradio as gr
 from langchain.llms import Ollama
 from langchain.prompts import ChatPromptTemplate
-from langchain.schema.runnable import RunnableMap, RunnablePassthrough
+from langchain.schema.runnable import RunnableLambda, RunnablePassthrough
+from langchain.document_loaders import TextLoader
+from langchain.embeddings.sentence_transformer import SentenceTransformerEmbeddings
+from langchain.text_splitter import CharacterTextSplitter
+from langchain.vectorstores import Chroma
 
-model = Ollama(model="dolphin2.2-mistral")
-prompt = ChatPromptTemplate.from_template(
+
+loader = TextLoader("./data/arquivo_entrada.txt")
+
+documents = loader.load()
+
+text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
+docs = text_splitter.split_documents(documents)
+
+# create the open-source embedding function
+embedding_function = SentenceTransformerEmbeddings(model_name="all-MiniLM-L6-v2")
+
+# load it into Chroma
+vectorstore = Chroma.from_documents(docs, embedding_function)
+
+retriever = vectorstore.as_retriever()
+
+template = """Answer the question based only on the following context:
+{context}
+
+Question: {question}
 """
-Now you are an occupational {safety agent} specialized in safety standards
-in industrial environments,in addition to knowing everything about epis and
-other subjects related to occupational safety and industrial environments
-that require a higher level of safety
-"""
+prompt = ChatPromptTemplate.from_template(template)
+
+model = Ollama(model="mistral:7b-instruct-q3_K_S")
+
+chain = (
+    {"context": retriever, "question": RunnablePassthrough()}
+    | prompt
+    | model
 )
-
-chain = {"safety agent": RunnablePassthrough()} | prompt | model
-
 
 def response(message, history):
     response = ""
@@ -25,8 +46,7 @@ def response(message, history):
 
 def main():
     gradio = gr.ChatInterface(response).queue()
-
-    gradio.launch()
+    gradio.launch(share=True)
 
 
 if __name__ == "__main__":
